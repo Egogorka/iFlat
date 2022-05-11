6a40bff1d45278fb2e4d41714b83a5033ab8b6f9
Egogorka <bike034@mail.ru>
2022-05-11 20:56:56 +0300

    fixed propagator function (differential equation)
    made some steps to change to double

--- code/solver/tracing.cpp
+++ code/solver/tracing.cpp
@@ -6,6 +6,7 @@
 
 #include "RaySolver.h"
 
+#include <cmath>
 #include <fstream>
 #include <string>
 
@@ -13,21 +14,53 @@ using scalar = float;
 using Vector4 = Vector<scalar,4>;
 
 scalar n(Vector<scalar,2> r){
-    return expf(r[1]);
+    return 1.25f+0.25f*tanhf(100*(1-r[1]));
 }
 
 Vector<scalar,2> dn(Vector<scalar,2> r){
-    return Vector<scalar,2>{0,expf(r[1])};
+    auto tmp = 5/coshf(100*(r[1]-1));
+    return Vector<scalar,2>{0,-tmp*tmp};
 }
+
+Vector<scalar,2> normalize(Vector<scalar,2> v){
+    return v/sqrtf(v*v);
+}
+
+const std::string PATH = "../data/points/";
 
 int tracing(){
 
-    auto solver = solver_basic(std::function{n},std::function{dn},1.0f,M_PI_2, 100, 1e-4, 1e+5);
+//    auto solver = solver_full(std::function{n},std::function{dn},1.0f,M_PI_2, 100, 1e-4, 1e+5);
 
-    for (auto& data : solver) {
-        std::cout << data.optical_length << '\n';
-        std::cout << data.start_angle << '\n';
+//    for(const auto& ray : solver){
+
+    float dt = 1e-5;
+
+
+    for(int j = -10; j <= 10; ++j ){
+        std::ofstream fout;
+        auto v = Vector<scalar,2>{sinf(M_PI_2 * j/10), -cosf(M_PI_2 * j/10)} * n(Vector2f{0.0f, 2.0f});
+
+        auto ray = solver_partial(n,dn,Vector2f{0.0f, 1.05f}, v, dt, int(.5f/dt));
+        auto values = ray.get_values();
+
+        std::string temp = PATH + "euler/" + std::to_string(10+j) + ".csv";
+
+        fout.open(temp);
+        for(int i = 0; i < values.size(); ++i){
+            auto r = Vector2f{values[i][0], values[i][1]};
+            auto v0 = Vector2f{values[i][2], values[i][3]};
+            auto t = dt * i;
+            // First integrals of the system:
+            // |v| ~ (v,v)/n(r)/n(r) const
+            // n*[v - (v,dn)/(dn,dn)*dn] ~ const
+
+            fout << t << ' ' << values[i] << ' ' << (v0*v0)/n(r)/n(r) << '\n';
+        }
+        fout << std::endl;
+        fout.close();
     }
+    //    }
 //    std::cout << "Number of points: " << solution.get_values().size() << '\n';
 //    std::cout << "Start angle: " << solution.start_angle() << '\n';
 //    std::cout << "End angle: " << solution.end_angle() << '\n';
