ae621f6b8e1062ea435bd42ef5a0a3f709c11111
AnalitikGnid <gladin.no@phystech.edu>
2022-05-18 21:17:18 +0300

    goodness via dist

--- code/goodness/goodness.cpp
+++ code/goodness/goodness.cpp
@@ -12,11 +12,15 @@ float start_angle(const std::vector<OpticalData> &optical_data, float h, float r
     return start_angle_calc;
 }
 
+float dist_sphere(float theta, float h, float r) {
+    return r * (-theta + asinf(((h + r) / r) * sinf(theta)));
+}
+
 float weight(int i, int N){
     return - 3.0f * pow(fabs((i - N / 2.0f) / N), 3) + 3.0f * pow((i - N / 2.0f) / N, 2);
 }
 
-float goodness(const std::vector<OpticalData> &optical_data, float h, float r, float alpha, int N){
+float goodness(const std::vector<OpticalData> &optical_data, float h, float r, float alpha, int N, bool f){
     std::ofstream fout;
     fout.open("outfile.txt", std::ios::app);
     float g = 0, theta;
@@ -26,9 +30,12 @@ float goodness(const std::vector<OpticalData> &optical_data, float h, float r, f
         return 1e5;
     }
     for (int i = 0; i < optical_data.size(); ++i) {
-        theta = alpha / 2 * (-1 + 2.0f * float(i) / float(N));
         if (std::isnan(optical_data[i].end_x)) return -1;
-        g += fabs(start_angle(optical_data, h, r, i) - theta) * weight(i, N);
+        theta = alpha / 2 * (-1 + 2.0f * float(i) / float(N));
+        if (f)
+            g += fabs(start_angle(optical_data, h, r, i) - theta) * weight(i, N);
+        else
+            g += fabs(dist_sphere(theta, h, r) - optical_data[i].end_x) * weight(i, N);
         norm += weight(i, N);
     }
     g /= optical_data.size() * norm;
